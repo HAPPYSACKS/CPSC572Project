13b408fea331415e3674bf68ebd863eb33d3c8bc
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2024-02-20 09:38:07 -0700

    first commit!

---
+++ airroutedatappend.py
@@ -0,0 +1,16 @@
+import requests
+
+api_key = 'AIzaSyAJ-57ihz0fENb-TEZF4wWCmKQtSCUdy9w'
+airport_code = 'JFK'  # Example airport code
+query = f'{airport_code} airport'
+url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={api_key}'
+
+response = requests.get(url)
+data = response.json()
+
+# Assuming the first result is the airport, extract its location
+print(data)
+latitude = data['results'][0]['geometry']['location']['lat']
+longitude = data['results'][0]['geometry']['location']['lng']
+
+print(f'Latitude: {latitude}, Longitude: {longitude}')
