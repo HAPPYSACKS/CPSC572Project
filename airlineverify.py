dbc8fc7a9ace2a76e3743ae8b198fe366ce21f59
HAPPYSACKS <74272531+HAPPYSACKS@users.noreply.github.com>
2024-04-04 21:41:17 -0600

    cleaned up comments

--- airlineverify.py
+++ airlineverify.py
@@ -2,15 +2,13 @@ import pandas as pd
 
 # Script that finds any `Airline Name` that has `Unknown`.
 
-
-# load the updated routes CSV file
 updated_routes_path = 'updated_routes.csv'  
 updated_df = pd.read_csv(updated_routes_path)
 
-# find rows where the 'Airline Name' is "Unknown"
+
 unknown_airlines_df = updated_df[updated_df['Airline Name'] == "Unknown"]
 
-# get unique airline names marked as "Unknown"
+
 unknown_airlines_list = unknown_airlines_df['airline'].unique()
 
 print("Airline Names marked as 'Unknown':", unknown_airlines_list)
